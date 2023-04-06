6e12279bf1030094dc8058f7936104d62d2b7905
erlendtyrmi <s132962@dtu.dk>
2023-04-06 14:13:56 +0200

    INIT Location tutorial
    Setup of basic loaction

---
+++ MysteryMeter/ContentView.swift
@@ -0,0 +1,38 @@
+//
+//  ContentView.swift
+//  MysteryMeter
+//
+//  Created by Erlend Tyrmi on 06/04/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @StateObject var locationDataManager = LocationDataManager()
+    var body: some View {
+        VStack {
+            switch locationDataManager.locationManager.authorizationStatus {
+            case .authorizedWhenInUse:  // Location services are available.
+                // Insert code here of what should happen when Location services are authorized
+                Text("Your current location is:")
+                Text("Latitude: \(locationDataManager.locationManager.location?.coordinate.latitude.description ?? "Error loading")")
+                Text("Longitude: \(locationDataManager.locationManager.location?.coordinate.longitude.description ?? "Error loading")")
+                
+            case .restricted, .denied:  // Location services currently unavailable.
+                // Insert code here of what should happen when Location services are NOT authorized
+                Text("Current location data was restricted or denied.")
+            case .notDetermined:        // Authorization not determined yet.
+                Text("Finding your location...")
+                ProgressView()
+            default:
+                ProgressView()
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MysteryMeter/MysteryMeterApp.swift
@@ -0,0 +1,17 @@
+//
+//  MysteryMeterApp.swift
+//  MysteryMeter
+//
+//  Created by Erlend Tyrmi on 06/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct MysteryMeterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
