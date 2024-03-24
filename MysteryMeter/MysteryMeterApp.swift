3186c44f26d63e84b0b12199b6a4dc0eb86e9a34
erlendtyrmi <s132962@dtu.dk>
2024-03-24 12:54:35 +0100

    Included firebase and updated app to ios 17

--- MysteryMeter/MysteryMeterApp.swift
+++ MysteryMeter/MysteryMeterApp.swift
@@ -6,9 +6,23 @@
 //
 
 import SwiftUI
+import FirebaseCore
+
+// The app delegate for firebase
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
 
 @main
 struct MysteryMeterApp: App {
+    // register app delegate for Firebase setup
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
     var body: some Scene {
         WindowGroup {
             ContentView()
